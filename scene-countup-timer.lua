57a46799b94bbe3c31ca32c89d89a2375f091fb1
Ryan Marsh <marshr483@gmail.com>
2024-09-01 11:54:53 -0400

    v0.4
    - Added more function comments
    - Removed unused global variables
    - Clarified some variable and function names
    - Added Display Note reset when there is no note source in the Program scene

--- scene-countup-timer.lua
+++ scene-countup-timer.lua
@@ -1,9 +1,8 @@
--- Scene CountUp Timer 0.3
---
--- Whenever a scene is changed, the script starts a count up timer and identifies the scene's name
-
 obs = obslua
+gversion = 0.4
+gdebug = 0
 
+gcur_seconds = 0
 gtime_text_source = ""
 gprogram_text_source = ""
 gpreview_text_source = ""
@@ -11,14 +10,9 @@ gnote_target = ""
 gspacer = ""
 gnote_prefix = ""
 
-ghave_timer = false
-
-gcur_seconds = 0
-gdebug = 0
-
-gversion = 0.3
 
 ----------------------------------------------------------
+-- Formatted logging messages
 local function logIt(name, msg)
 
 	if msg ~= nil then
@@ -41,11 +35,11 @@ function dumpSortedAttrs(obj, label)
 end
 
 ----------------------------------------------------------
-function updateTextSource(whichSource, msg_text)
+function updateTextSource(editSource, msg_text)
 	--logIt('in updateTextSource', msg_text)
 
-	local source = obs.obs_get_source_by_name(whichSource)
-    --logIt('   whichSource', whichSource)
+	local source = obs.obs_get_source_by_name(editSource)
+    --logIt('   editSource', editSource)
     --logIt('   source', source)
 
 	if source ~= nil then
@@ -66,15 +60,16 @@ function getTimeText()
 	local seconds 			= math.floor(gcur_seconds % 60)
 	local total_minutes	= math.floor(gcur_seconds / 60)
 	local minutes				= math.floor(total_minutes % 60)
-	local hours					= math.floor(total_minutes / 60)
+	--local hours					= math.floor(total_minutes / 60)
 	local text					= string.format("%02d:%02d", minutes, seconds)
+	--local text					= string.format("%02:%02d:%02d", hours, minutes, seconds)
 
 	return text
 end
 
 ----------------------------------------------------------
 function timer_callback()
-	--log_it('in timer_callback')
+	--logIt('in timer_callback')
 
 	gcur_seconds = gcur_seconds + 1
 	local msg_text = getTimeText()
@@ -83,36 +78,44 @@ function timer_callback()
 end
 
 ----------------------------------------------------------
-function starts(str, prefix)
-   return str:sub(1, #prefix) == prefix
+-- Check if the source name starts with the user-defined Note prefix
+function noteCheck(str)
+   return str:sub(1, #gnote_prefix) == gnote_prefix
 end
 
 ----------------------------------------------------------
+-- Update the user-defined Note Display source
 function updateNote()
 	--logIt('updateNote', "")
 	updateTextSource(gnote_target, "")
 	local sources = obs.obs_enum_sources()
+	local noteFound = false
 
 	if sources ~= nil then
 		for _, source in ipairs(sources) do
-
 			if obs.obs_source_active(source) then
 				local name = obs.obs_source_get_name(source)
 				logIt('updateNote:name', name)
 
-				if starts(name, gnote_prefix) then
+				if noteCheck(name) then
 					source_id = obs.obs_source_get_unversioned_id(source)
 
 					if source_id == "text_gdiplus" or source_id == "text_ft2_source" then
 						local settings = obs.obs_source_get_settings(source)
 						msg_text = obs.obs_data_get_string(settings, "text")
 						updateTextSource(gnote_target, msg_text)
+						noteFound = true
 						logIt('Update Note', msg_text)
 
 						obs.obs_data_release(settings)
+						break
 					end
-
 				end
+
+				if not noteFound then
+					updateTextSource(gnote_target, "")
+				end
+
 			end
 		end
 	end
@@ -150,15 +153,14 @@ end
 ----------------------------------------------------------
 -- Defines properties that the user can change for the entire script module itself
 function script_properties()
-
 	logIt('in script_properties')
 
 	local props = obs.obs_properties_create()
 	local time_s = obs.obs_properties_add_list(props, "gtime_text_source", "Timer ", obs.OBS_COMBO_TYPE_EDITABLE, obs.OBS_COMBO_FORMAT_STRING)
 	local prog_s = obs.obs_properties_add_list(props, "gprogram_text_source", "Program ", obs.OBS_COMBO_TYPE_EDITABLE, obs.OBS_COMBO_FORMAT_STRING)
 	local prev_s = obs.obs_properties_add_list(props, "gpreview_text_source", "Preview ", obs.OBS_COMBO_TYPE_EDITABLE, obs.OBS_COMBO_FORMAT_STRING)
-	local prefix_s = obs.obs_properties_add_text(props, "gnote_prefix", "Note source prefix ", obs.OBS_TEXT_DEFAULT)
-	local note_s = obs.obs_properties_add_list(props, "gnote_target", "Note display ", obs.OBS_COMBO_TYPE_EDITABLE, obs.OBS_COMBO_FORMAT_STRING)
+	local prefix_s = obs.obs_properties_add_text(props, "gnote_prefix", "Note Source prefix ", obs.OBS_TEXT_DEFAULT)
+	local note_s = obs.obs_properties_add_list(props, "gnote_target", "Note Display ", obs.OBS_COMBO_TYPE_EDITABLE, obs.OBS_COMBO_FORMAT_STRING)
 
 	local sources = obs.obs_enum_sources()
 	if sources ~= nil then
@@ -179,15 +181,12 @@ function script_properties()
 	return props
 end
 
-----------------------------------------------------------
--- Defines the description of this script in the Scripts window
+-- Define the Description in the Scripts window
 function script_description()
-	msg = string.format("Sets a text source to act as a countup timer when a scene is changed. (ver: %s)", tostring(gversion))
-
+	msg = string.format("Set text sources to react when Program or Preview scenes are changed (ver: %s)", tostring(gversion))
 	return msg
 end
 
-----------------------------------------------------------
 function script_update(settings)
 	logIt('in script_update')
 
@@ -196,10 +195,8 @@ function script_update(settings)
 	gpreview_text_source	= obs.obs_data_get_string(settings, "gpreview_text_source")
 	gnote_prefix					= obs.obs_data_get_string(settings, "gnote_prefix")
 	gnote_target					= obs.obs_data_get_string(settings, "gnote_target")
-
 end
 
-----------------------------------------------------------
 function script_load(settings)
 	logIt('--------\nin script_load')
 
@@ -207,7 +204,6 @@ function script_load(settings)
 
 	obs.obs_frontend_add_event_callback(onFrontendEvent)
 	onFrontendEvent(obs.OBS_FRONTEND_EVENT_SCENE_CHANGED)
-
 end
 
 function script_unload()
